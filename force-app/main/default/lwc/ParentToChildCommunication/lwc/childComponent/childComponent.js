3ad816f24e92912ee143e23e6d7f893f60ba9b6f
Shubham Band <shubham.band58@gmail.com>
2024-08-07 17:35:40 +0530

    Non Primitive data sends from parent to child

--- force-app/main/default/lwc/ParentToChildCommunication/lwc/childComponent/childComponent.js
+++ force-app/main/default/lwc/ParentToChildCommunication/lwc/childComponent/childComponent.js
@@ -1,5 +1,7 @@
+
 import { LightningElement,api } from 'lwc';
 
 export default class ChildComponent extends LightningElement {
     @api parent;
+    @api crousalDetails;
 }
--- force-app/main/default/lwc/ParentToChildCommunication/lwc/parentComponent/parentComponent.js
+++ force-app/main/default/lwc/ParentToChildCommunication/lwc/parentComponent/parentComponent.js
@@ -3,6 +3,25 @@ import { LightningElement } from 'lwc';
 export default class ParentComponent extends LightningElement {
     name;
     dispatch;
+    crousalData = [
+        {
+            src : "https://www.lightningdesignsystem.com/assets/images/carousel/carousel-01.jpg",
+            header : "First Card",
+            description: "First card description."
+        },
+        {
+            src : "https://www.lightningdesignsystem.com/assets/images/carousel/carousel-02.jpg",
+            header : "Second Card",
+            description: "Second card description."
+        },
+        {
+            src : "https://www.lightningdesignsystem.com/assets/images/carousel/carousel-03.jpg",
+            header : "Third Card",
+            description: "Third card description."
+        }
+    ];
+
+
     handleChange(event){
         this.name =event.detail.value;
         console.log(this.name);
@@ -13,4 +32,7 @@ export default class ParentComponent extends LightningElement {
         this.dispatch = this.name;
         console.log('dispatch value from parent=>',this.dispatch);
     }
+
+
+
 }
